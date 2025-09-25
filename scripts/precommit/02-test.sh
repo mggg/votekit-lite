491bf3616cd456c52e9d893323d494c20f241b54
nofurtherinformation <dhalpern@uchicago.edu>
2025-09-25 14:38:08 -0500

    Add python test

--- scripts/precommit/02-test.sh
+++ scripts/precommit/02-test.sh
@@ -3,5 +3,8 @@ set -euo pipefail
 
 COMPOSE_CMD="${COMPOSE_CMD:-docker compose}"
 
-echo "② $COMPOSE_CMD exec -T app pnpm test"
+echo "$COMPOSE_CMD exec -T app pnpm test"
 $COMPOSE_CMD exec -T app pnpm test
+
+echo "$COMPOSE_CMD exec -T lambda python main.py -v"
+$COMPOSE_CMD exec -T lambda python main.py -v
