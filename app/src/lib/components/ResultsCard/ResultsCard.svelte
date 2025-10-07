c32bcf8ae8fcab90002f3593ff50dc4e4814de7f
Dylan <dhalpern@uchicago.edu>
2025-10-07 12:48:54 -0500

    UI cleanup (#38)
    * Clean up option cards
    
    * Add voter behavior details
    
    * Result card utils
    
    * Update ResultsCard.svelte
    
    * Import config from json
    
    * Update ResultsCard.svelte
    
    * Remove default name generator
    
    * Clean up very small cohesion values
    
    * Clear file input value after ingestion
    
    * Add links and clean up tooltip
    
    * Population loading from config fix
    
    * Don't modify total population
    
    * Clean up CSV download
    
    * Keep focus on copy

--- app/src/lib/components/ResultsCard/ResultsCard.svelte
+++ app/src/lib/components/ResultsCard/ResultsCard.svelte
@@ -5,9 +5,18 @@
 	import ResultsTable from './ResultsTable.svelte';
 	import ResultCardDetails from './ResultCardDetails.svelte';
 	import Loading from '$lib/components/Loading.svelte';
+	import { shareUrl } from '$lib/utils/shareUrl';
+	import GearIcon from './GearIcon.svelte';
+	import { downloadResults } from '$lib/utils/downloadResults';
+	import { downloadSvg } from '$lib/utils/downloadSvg';
+	import { downloadPng } from '$lib/utils/downloadPng';
+	import { downloadJson } from '$lib/utils/downloadJson';
+
 	const { runId } = $props<{ runId: string }>();
 	let activeTab = $state('histogram');
 	const run = $derived(resultsState.runs.find((r) => r.id === runId));
+	let showCopiedUrl = $state(false);
+	let hovered = $state(false);
 
 	$effect(() => {
 		// If data is incomplete, check the results
@@ -15,11 +24,21 @@
 			resultsState.checkStaleRun(runId);
 		}
 	});
+	$effect(() => {
+		if (showCopiedUrl) {
+			setTimeout(() => (showCopiedUrl = false), 2000);
+		}
+	});
 </script>
 
 <!-- name of each tab group should be unique -->
 {#if run}
-	<div class="flex flex-col gap-2">
+	<div
+		class="relative flex flex-col gap-2"
+		onmouseenter={() => (hovered = true)}
+		onmouseleave={() => (hovered = false)}
+		role="group"
+	>
 		<div class="tabs-lift tabs">
 			<input
 				type="radio"
@@ -30,7 +49,10 @@
 				onchange={() => (activeTab = 'histogram')}
 				style={`background: ${activeTab === 'histogram' ? 'white' : 'none'}`}
 			/>
-			<div class="relative tab-content border-base-300 bg-base-100 p-6">
+			<div
+				class="relative tab-content border-base-300 bg-base-100 p-6"
+				id={`histogram-container-${runId}`}
+			>
 				<ResultCardHeader {run} />
 				{#if run.result}
 					<ResultsChart {runId} />
@@ -50,7 +72,7 @@
 				onchange={() => (activeTab = 'table')}
 				style={`background: ${activeTab === 'table' ? 'white' : 'none'}`}
 			/>
-			<div class="tab-content border-base-300 bg-base-100 p-6">
+			<div class="tab-content border-base-300 bg-base-100 p-6" id={`table-${runId}`}>
 				<ResultCardHeader {run} />
 				{#if run.result}
 					<ResultsTable {run} />
@@ -74,7 +96,99 @@
 				<ResultCardDetails {run} />
 			</div>
 		</div>
+		<!-- svelte-ignore a11y_no_noninteractive_tabindex -->
+		<div
+			tabindex="0"
+			id="dropdown-results-{runId}"
+			class={`dropdown absolute top-0 right-0 transition-opacity duration-300 ${hovered ? 'opacity-100' : 'opacity-0'}`}
+		>
+			<div role="button" class="btn p-0 btn-ghost btn-md">
+				<GearIcon />
+			</div>
+			<!-- svelte-ignore a11y_no_noninteractive_tabindex -->
+			<ul tabindex="0" class="dropdown-content menu z-1 w-52 rounded-box bg-base-100 p-2 shadow-xl">
+				<li>
+					<button
+						class="btn btn-ghost"
+						onclick={() => {
+							resultsState.loadSimulationSettings(run.id);
+						}}>Load simulation settings</button
+					>
+				</li>
+				{#if showCopiedUrl}
+					<li><p class="text-info">Share URL copied to clipboard!</p></li>
+				{:else}
+					<li>
+						<button
+							class="btn btn-ghost"
+							onclick={(e) => {
+								e.preventDefault();
+								shareUrl(run.id);
+								showCopiedUrl = true;
+								// prevent focus from leaving the dropdown
+								const dropdown = document.getElementById(`dropdown-results-${runId}`);
+								if (dropdown) {
+									dropdown.focus();
+								}
+							}}>Share simulation results</button
+						>
+					</li>
+				{/if}
+				<li>
+					<button class="btn btn-ghost" onclick={() => downloadResults(run)}
+						>Download results (CSV)</button
+					>
+				</li>
+				<li>
+					<button
+						class="btn btn-ghost"
+						onclick={() => {
+							activeTab = 'histogram';
+							setTimeout(() => {
+								const el = document.getElementById(`histogram-container-${runId}`);
+								if (el) {
+									downloadSvg(el, run.id);
+								}
+							}, 125);
+						}}>Download chart (SVG)</button
+					>
+				</li>
+				<li>
+					<button
+						class="btn btn-ghost"
+						onclick={() => {
+							activeTab = 'histogram';
+							setTimeout(() => {
+								const el = document.getElementById(`histogram-container-${runId}`);
+								if (el) {
+									downloadPng(el, run.id);
+								}
+							}, 125);
+						}}>Download chart (PNG)</button
+					>
+				</li>
+				<li>
+					<button class="btn btn-ghost" onclick={() => downloadJson(run.config, run.id)}
+						>Download configuration (JSON)</button
+					>
+				</li>
+				<li>
+					<button class="btn btn-ghost" onclick={() => resultsState.removeRun(run.id)}
+						>Delete this run</button
+					>
+				</li>
+			</ul>
+		</div>
 	</div>
 {:else}
 	<p>No run found</p>
 {/if}
+
+<style>
+	button {
+		text-align: left;
+		font-weight: normal;
+		align-items: left;
+		justify-content: start;
+	}
+</style>
--- app/src/lib/stores/formStore.svelte.ts
+++ app/src/lib/stores/formStore.svelte.ts
@@ -1,4 +1,3 @@
-import { randomRunName } from '$lib';
 import { goto } from '$app/navigation';
 // Re-export types from storage for convenience
 import type { Slate, VoterBloc } from './types';
@@ -90,9 +89,7 @@ export class FormState {
 	seatsMax: number = $derived(Math.max(this.seatsMin, this.totalCandidates));
 
 	turnstileToken: string = $state('');
-	initialize() {
-		this.name = randomRunName();
-	}
+	initialize() {}
 
 	updateNumSlates(value: number) {
 		if (this.totalCandidates == MAX_CANDIDATES && value >= this.slates.length) {
@@ -245,6 +242,33 @@ export class FormState {
 		}).then((res) => (res.ok ? res.json() : null));
 		this.turnstileToken = '';
 		resultsState.listenForResults(id, config);
+	}
+
+	loadSimulationSettings(config: VotekitConfig) {
+		this.unallocatedPopulation = 0;
+		this.name = config.name + ' (Edited)';
+		this.trials = config.trials;
+		this.system = config.election.system;
+		this.ballotGenerator = config.ballotGenerator;
+		this.numSeats = config.election.numSeats;
+		this.maxRankingCandidatesInput = config.election.maxBallotLength;
+		this.numVoterBlocs = Object.keys(config.voterBlocs).length;
+		this.blocs = Object.entries(config.voterBlocs).map(([name, bloc]) => ({
+			name,
+			population: bloc.proportion * config.numVoters,
+			turnout: 1.0
+		}));
+		this.slates = Object.entries(config.slates).map(([name, slate]) => ({
+			name,
+			numCandidates: slate.numCandidates
+		}));
+		this.blocPreferences = Object.entries(config.voterBlocs).map(([name, bloc]) =>
+			this.slates.map((slate) => bloc.preference[slate.name] ?? 'all_bets_off')
+		);
+		this.blocCohesion = Object.entries(config.voterBlocs).map(([name, bloc]) =>
+			this.slates.map((slate) => Math.round((bloc.cohesion[slate.name] ?? 0) * 100) / 100)
+		);
+		goto(`/run`);
 	}
 }
 
