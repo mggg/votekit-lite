f7d8b97a599066838a08660b115f5dafcf238b89
Chris Donnay <christopherdonnay@gmail.com>
2025-10-16 09:28:52 -0400

    add tour descriptions

--- app/src/lib/stores/tourStore.svelte.ts
+++ app/src/lib/stores/tourStore.svelte.ts
@@ -12,8 +12,50 @@ const TOUR_STEPS: Array<Step> = [
 	},
 	{
 		title: 'Election Details',
-		description: 'Enter the election details.',
+		description:
+			'Enter the election details. This is where you decide how many groups of candidates\
+		there are, how many seats are up for election, and how many candidates can be ranked on a ballot.',
 		id: 'election-details'
+	},
+	{
+		title: 'Voter Blocs',
+		description: 'Enter information about different groups of voters, called blocs.',
+		id: 'voter-blocs'
+	},
+	{
+		title: 'Basic Settings',
+		description: 'Enter the number and size of the blocs.',
+		id: 'voter-blocs-basic-settings'
+	},
+	{
+		title: 'Candidate Strength',
+		description: 'Decide how the blocs feel about individual candidates.',
+		id: 'voter-blocs-candidate-strength'
+	},
+	{
+		title: 'Voter Cohesion',
+		description: 'Decide how the blocs feel about individual slates.',
+		id: 'voter-blocs-cohesion'
+	},
+	{
+		title: 'Voter Behavior',
+		description: 'Decide how the blocs behave when ranking.',
+		id: 'voter-behavior'
+	},
+	{
+		title: 'Run Details',
+		description: 'Enter a name for the run and the number of elections to simulate.',
+		id: 'run-details'
+	},
+	{
+		title: 'Preview',
+		description: 'See a preview of the parameters you have entered.',
+		id: 'preview'
+	},
+	{
+		title: 'Run Simulation',
+		description: 'Click to run the simulation.',
+		id: 'run-simulation-button'
 	}
 ];
 
